63058eef80cf3908422decf0fca26a2f5f0e7009
Jörg Grote <joerg@grote-online.de>
2017-12-06 10:58:45 +0100

    fix

--- smalltools/connector/smtp/connector.module.ts
+++ smalltools/connector/smtp/connector.module.ts
@@ -6,7 +6,7 @@
 import { HttpModule } from "@angular/http";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import {TibcoAPILayerConnectorContribution} from "./connector";
+import {TibcoSMTPConnectorContribution} from "./connector";
 import {WiServiceContribution} from "wi-studio/app/contrib/wi-contrib";
 
 @NgModule({
