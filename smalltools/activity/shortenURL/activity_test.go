59167ee76349e510770fbcea886f6ecf0af3f21f
Jörg Grote <joerg@grote-online.de>
2018-01-09 15:40:27 +0100

    added draft of shorten URL

---
+++ smalltools/activity/shortenURL/activity_test.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright © 2017. TIBCO Software Inc.
+ * This file is subject to the license terms contained
+ * in the license file that is distributed with this file.
+ */
+package shortenURL
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/stretchr/testify/assert"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+	return activityMetadata
+}
+
+func TestActivityRegistration(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	if act == nil {
+		t.Error("Activity Not Registered")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(act.Metadata())
+	//setup attrs
+	tc.SetInput("longURL", "http://www.GODev.de")
+
+	_, err := act.Eval(tc)
+	assert.Nil(t, err)
+
+	result := tc.GetOutput("shortURL")
+	assert.Equal(t, "http://www.GODev.de", result)
+
+	t.Log(result)
+}
